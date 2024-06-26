3f25f268f410b794b026b14ed9290cd6cfe42024
Tuğsem Saranlı <saranlitugsem@gmail.com>
2024-06-26 17:17:38 +0300

    add route

--- src/config/api.js
+++ src/config/api.js
@@ -7,6 +7,7 @@ const PATIENT_URL = `${HOST}/api/v1/patients`;
 const LOGIN_URL = `${HOST}/login`;
 const USER_URL = `${HOST}/me`;
 const LOGOUT_URL = `${HOST}/logout`;
+const SIGNUP_URL = `${HOST}/signup`;
 
 export {
   STOCK_LOG_URL, STOCK_URL, LOGIN_URL, USER_URL, LOGOUT_URL, SIGNUP_URL, PATIENT_URL,
