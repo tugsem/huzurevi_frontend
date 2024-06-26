8dadfbbdf75e525e868ab0453905ea332f762f9c
Tuğsem Saranlı <saranlitugsem@gmail.com>
2024-06-26 16:56:39 +0300

    add host domain

--- src/config/api.js
+++ src/config/api.js
@@ -1,13 +1,13 @@
-// const STOCK_URL = 'https://huzurevibackend-development.up.railway.app/api/v1/stocks/';
-// const STOCK_LOG_URL = 'https://huzurevibackend-development.up.railway.app/api/v1/stock_logs/';
-const STOCK_URL = '/api/v1/stocks';
-const STOCK_LOG_URL = '/api/v1/stock_logs';
+const HOST = 'https://huzurevibackend-development.up.railway.app';
 
-const PATIENT_URL = '/api/v1/patients';
-const LOGIN_URL = '/login';
-const USER_URL = '/me';
-const LOGOUT_URL = '/logout';
-const SIGNUP_URL = '/signup';
+const STOCK_URL = `${HOST} + '/api/v1/stocks'`;
+const STOCK_LOG_URL = `${HOST} + '/api/v1/stock_logs'`;
+
+const PATIENT_URL = `${HOST} + '/api/v1/patients'`;
+const LOGIN_URL = `${HOST} + '/login'`;
+const USER_URL = `${HOST} + '/me'`;
+const LOGOUT_URL = `${HOST} + '/logout'`;
+const SIGNUP_URL = `${HOST} + '/signup'`;
 
 export {
   STOCK_LOG_URL, STOCK_URL, LOGIN_URL, USER_URL, LOGOUT_URL, SIGNUP_URL, PATIENT_URL,
