8cbff1e8720e39503e097f4f608d38969d0852b7
Tuğsem Saranlı <saranlitugsem@gmail.com>
2024-06-26 17:11:16 +0300

    fix endpoints

--- src/config/api.js
+++ src/config/api.js
@@ -1,13 +1,12 @@
 const HOST = 'https://huzurevibackend-development.up.railway.app';
 
-const STOCK_URL = `${HOST} + '/api/v1/stocks'`;
-const STOCK_LOG_URL = `${HOST} + '/api/v1/stock_logs'`;
+const STOCK_URL = `${HOST}/api/v1/stocks`;
+const STOCK_LOG_URL = `${HOST}/api/v1/stock_logs`;
 
-const PATIENT_URL = `${HOST} + '/api/v1/patients'`;
-const LOGIN_URL = `${HOST} + '/login'`;
-const USER_URL = `${HOST} + '/me'`;
-const LOGOUT_URL = `${HOST} + '/logout'`;
-const SIGNUP_URL = `${HOST} + '/signup'`;
+const PATIENT_URL = `${HOST}/api/v1/patients`;
+const LOGIN_URL = `${HOST}/login`;
+const USER_URL = `${HOST}/me`;
+const LOGOUT_URL = `${HOST}/logout`;
 
 export {
   STOCK_LOG_URL, STOCK_URL, LOGIN_URL, USER_URL, LOGOUT_URL, SIGNUP_URL, PATIENT_URL,
